5e76fa492b0095269f07e479ed913b1a432f8d12
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-26 14:31:43 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,11 +3,3 @@ module Findable
     self.all.detect{|a| a.name == name}
   end
 end
-#
-# def self.find_by_name(name)
-#   @@artists.detect{|a| a.name == name}
-# end
-#
-# def self.find_by_name(name)
-#   @@songs.detect{|a| a.name == name}
-# end
