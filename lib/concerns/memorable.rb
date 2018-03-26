f6a6c61e0bad7f2436ebcb1b761c66edb52b560a
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-26 14:26:43 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,7 +1,7 @@
 module Memorable
 
   def reset_all
-
+    self.all.clear
   end
 
   def count
@@ -24,7 +24,7 @@ end
   # def self.reset_all
   #   @@songs.clear
   # end
-  
+
   # def self.count
   #   self.all.count
   # end
